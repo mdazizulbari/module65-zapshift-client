c46b6349ee6bb4ac27f6c67a55c7620e8a9e2318
dmabari <mdazizulbarishafin@gmail.com>
2025-07-02 23:27:54 +0600

    Add assets and components for ProFast branding and warehouse data
    - Added a new logo component for ProFast with an image and title.
    - Introduced a JSON file containing warehouse information across various regions, including coordinates and flowchart links.
    - Added a new PNG image for the ProFast logo.

---
+++ src/pages/shared/ProFastLogo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "../../assets/logo.png";
+
+const ProFastLogo = () => {
+  return (
+    <div className="flex">
+      <img src={logo} alt="" />
+      <p className="text-3xl font-bold">ProFast</p>
+    </div>
+  );
+};
+
+export default ProFastLogo;
