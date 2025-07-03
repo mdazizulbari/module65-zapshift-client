3d45a130c6887b4d10959df67e664f84daa5ac06
dmabari <mdazizulbarishafin@gmail.com>
2025-07-04 00:12:30 +0600

    feat: add AuthLayout and Login components; update router to include authentication routes

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -1,14 +1,19 @@
 import { createBrowserRouter } from "react-router";
 import RootLayout from "../layouts/RootLayout";
 import Home from "../pages/home/Home";
+import AuthLayout from "../layouts/AuthLayout";
+import Login from "../pages/Authentication/Login";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     Component: RootLayout,
     // loader: loadRootData,
-    children:[
-        {index:true,element:<Home/>}
-    ]
+    children: [{ index: true, element: <Home /> }],
+  },
+  {
+    path: "/",
+    Component: AuthLayout,
+    children: [{ path: "login", element: <Login /> }],
   },
 ]);
