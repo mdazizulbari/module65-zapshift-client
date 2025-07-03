78f09d997c01ca0333e177a9784b39d11991bbba
dmabari <mdazizulbarishafin@gmail.com>
2025-07-04 01:50:31 +0600

    feat: add react-hook-form for form handling in Login component; update AuthLayout for improved layout

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { Outlet } from "react-router";
 import authImage from "../assets/authImage.png";
+import ProFastLogo from "../pages/shared/ProFastLogo";
 
 const AuthLayout = () => {
   return (
-    <div className="bg-base-200">
+    <div className="bg-base-200 p-12">
+      <div className="">
+        <ProFastLogo />
+      </div>
       <div className="hero-content flex-col lg:flex-row-reverse">
-        <img src={authImage} className="max-w-sm rounded-lg shadow-2xl" />
-        <div>
+        <div className="flex-1">
+          <img src={authImage} className="max-w-sm rounded-lg shadow-2xl" />
+        </div>
+        <div className="flex-1">
           <Outlet />
         </div>
       </div>
--- src/pages/Authentication/Login.jsx
+++ src/pages/Authentication/Login.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
-    return (
-        <div>
-            Login
-        </div>
-    );
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <form action="" onSubmit={handleSubmit(onSubmit)}>
+        <fieldset className="fieldset">
+          <label className="label">Email</label>
+          <input
+            type="email"
+            className="input"
+            {...register("email")}
+            placeholder="Email"
+          />
+
+          <label className="label">Password</label>
+          <input
+            type="password"
+            {...register("password", { required: true, minLength: 6 })}
+            className="input"
+            placeholder="Password"
+          />
+
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button className="btn btn-neutral mt-4">Login</button>
+        </fieldset>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
