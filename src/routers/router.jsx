78256953152371780c29fa1501abbcd5b83dda31
dmabari <mdazizulbarishafin@gmail.com>
2025-07-04 04:02:11 +0600

    feat: add Register component and update router for registration path

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -3,6 +3,7 @@ import RootLayout from "../layouts/RootLayout";
 import Home from "../pages/home/Home";
 import AuthLayout from "../layouts/AuthLayout";
 import Login from "../pages/Authentication/Login";
+import Register from "../pages/Authentication/Register";
 
 export const router = createBrowserRouter([
   {
@@ -14,6 +15,9 @@ export const router = createBrowserRouter([
   {
     path: "/",
     Component: AuthLayout,
-    children: [{ path: "login", element: <Login /> }],
+    children: [
+      { path: "login", element: <Login /> },
+      { path: "register", element: <Register /> },
+    ],
   },
 ]);
