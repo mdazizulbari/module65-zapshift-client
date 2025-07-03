8fe996c965a0a7668ad7b4a64cabed64690a6634
dmabari <mdazizulbarishafin@gmail.com>
2025-07-03 17:52:16 +0600

    chore: update package dependencies and maintain package-lock.json

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
 import { router } from "./routers/router.jsx";
+import "aos/dist/aos.css";
+import { AOS } from "aos";
+
+AOS.init();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
