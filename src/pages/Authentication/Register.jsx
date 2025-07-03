86d6ebb5183fe152ada1f7886b6fc0fdb9027336
dmabari <mdazizulbarishafin@gmail.com>
2025-07-04 04:04:55 +0600

    feat: implement Register component with form handling and validation

--- src/pages/Authentication/Register.jsx
+++ src/pages/Authentication/Register.jsx
@@ -1,11 +1,53 @@
-import React from 'react';
+import React from "react";
+import { useForm } from "react-hook-form";
 
 const Register = () => {
-    return (
-        <div>
-            register
-        </div>
-    );
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <form action="" onSubmit={handleSubmit(onSubmit)}>
+        <fieldset className="fieldset">
+          <label className="label">Email</label>
+          <input
+            type="email"
+            className="input"
+            {...register("email")}
+            placeholder="Email"
+          />
+
+          <label className="label">Password</label>
+          <input
+            type="password"
+            {...register("password", { required: true, minLength: 6 })}
+            className="input"
+            placeholder="Password"
+          />
+          {errors.password?.type === "minLength" && (
+            <p className="text-red-500">
+              Password must be 6 characters or longer
+            </p>
+          )}
+          {errors.password?.type === "required" && (
+            <p className="text-red-500">Password is required</p>
+          )}
+
+          <div>
+            <a className="link link-hover">Forgot password?</a>
+          </div>
+          <button className="btn w-fit btn-neutral mt-4">Login</button>
+        </fieldset>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
