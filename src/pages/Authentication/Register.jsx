487f86a7541e367bb2a17f255767a1770f53de2a
dmabari <mdazizulbarishafin@gmail.com>
2025-07-04 16:08:28 +0600

    feat: restructure Register component layout for improved readability and styling

--- src/pages/Authentication/Register.jsx
+++ src/pages/Authentication/Register.jsx
@@ -22,49 +22,54 @@ const Register = () => {
   };
 
   return (
-    <div>
-      <form action="" onSubmit={handleSubmit(onSubmit)}>
-        <fieldset className="fieldset">
-          <label className="label">Email</label>
-          <input
-            type="email"
-            className="input"
-            {...register("email")}
-            placeholder="Email"
-          />
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-5xl font-bold">Login now!</h1>
+        </div>
+        <form action="" onSubmit={handleSubmit(onSubmit)}>
+          <fieldset className="fieldset">
+            <label className="label">Email</label>
+            <input
+              type="email"
+              className="input"
+              {...register("email")}
+              placeholder="Email"
+            />
 
-          <label className="label">Password</label>
-          <input
-            type="password"
-            {...register("password", { required: true, minLength: 6 })}
-            className="input"
-            placeholder="Password"
-          />
-          {errors.password?.type === "minLength" && (
-            <p className="text-red-500">
-              Password must be 6 characters or longer
-            </p>
-          )}
-          {errors.password?.type === "required" && (
-            <p className="text-red-500">Password is required</p>
-          )}
+            <label className="label">Password</label>
+            <input
+              type="password"
+              {...register("password", { required: true, minLength: 6 })}
+              className="input"
+              placeholder="Password"
+            />
+            {errors.password?.type === "minLength" && (
+              <p className="text-red-500">
+                Password must be 6 characters or longer
+              </p>
+            )}
+            {errors.password?.type === "required" && (
+              <p className="text-red-500">Password is required</p>
+            )}
 
-          <div>
-            <a className="link link-hover">Forgot password?</a>
-          </div>
-          <button className="btn btn-primary text-black w-fit btn-neutral mt-4">
-            Register
-          </button>
-        </fieldset>
-        <p>
-          <small>
-            Already have an account?{" "}
-            <Link to={"/login"} className="text-primary">
-              Login
-            </Link>
-          </small>
-        </p>
-      </form>
+            <div>
+              <a className="link link-hover">Forgot password?</a>
+            </div>
+            <button className="btn btn-primary text-black btn-neutral mt-4">
+              Register
+            </button>
+          </fieldset>
+          <p>
+            <small>
+              Already have an account?{" "}
+              <Link to={"/login"} className="btn-link btn">
+                Login
+              </Link>
+            </small>
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
