2353f145b037b98f18aacfcf7643bea60ad25407
dmabari <mdazizulbarishafin@gmail.com>
2025-07-04 20:37:45 +0600

    feat: add SocialLogin component to Login page

--- src/pages/Authentication/Login.jsx
+++ src/pages/Authentication/Login.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router";
+import SocialLogin from "./SocialLogin";
 
 const Login = () => {
   const {
@@ -57,6 +58,7 @@ const Login = () => {
             </small>
           </p>
         </form>
+        <SocialLogin />
       </div>
     </div>
   );
