4b0054ee3abdf17adeaf4eccda742d8440174e42
smabari <mdazizulbarishafin@gmail.com>
2025-08-02 05:39:36 +0600

    module 66-6

--- src/pages/Dashboard/Payment/PaymentForm.jsx
+++ src/pages/Dashboard/Payment/PaymentForm.jsx
@@ -3,12 +3,16 @@ import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 import { useParams } from "react-router";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import useAuth from "../../../hooks/useAuth";
 
 const PaymentForm = () => {
   const axiosSecure = useAxiosSecure();
   const stripe = useStripe();
   const elements = useElements();
+  const { user } = useAuth();
   const [error, setError] = useState();
+  const [processing, setProcessing] = useState(false);
+  const [succeeded, setSucceeded] = useState(false);
   const { parcelId } = useParams();
   console.log(parcelId);
 
@@ -25,6 +29,8 @@ const PaymentForm = () => {
   }
   console.log(parcelInfo);
   const amount = parcelInfo.deliveryCost;
+  const amountInCents = amount * 100;
+  console.log(amountInCents);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -37,6 +43,7 @@ const PaymentForm = () => {
       return;
     }
 
+    // step-1 validate the card
     const { error, paymentMethod } = await stripe.createPaymentMethod({
       type: "card",
       card,
@@ -47,6 +54,41 @@ const PaymentForm = () => {
     } else {
       setError("");
       console.log("paymentMethod", paymentMethod);
+
+      // step-2: create payment intent
+      const res = await axiosSecure.post("/create-payment-intent", {
+        amountInCents,
+        parcelId,
+      });
+
+      const clientSecret = res.data.clientSecret;
+      console.log("🔑 Client Secret from backend:", clientSecret);
+      console.log("Full response from backend:", res);
+
+      // step-3 confirm payment
+      const result = await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: elements.getElement(CardElement),
+          billing_details: {
+            name: user.displayName,
+            email: user.email,
+          },
+        },
+      });
+
+      if (result.error) {
+        console.log(result.error.message);
+        setError(`Payment failed: ${result.error.message}`);
+        setProcessing(false);
+      } else {
+        console.log(result);
+        if (result.paymentIntent.status === "succeeded") {
+          setError(null);
+          setProcessing(false);
+          setSucceeded(true);
+          // step-4 mark parcel paid also create payment history
+        }
+      }
     }
   };
 
