6f8cd4e12098b576f11aa6e399d49077c25dca61
smabari <mdazizulbarishafin@gmail.com>
2025-08-03 07:04:00 +0600

    module 67-1

--- src/pages/Authentication/Register.jsx
+++ src/pages/Authentication/Register.jsx
@@ -3,6 +3,7 @@ import { useForm } from "react-hook-form";
 import useAuth from "../../hooks/useAuth";
 import { Link } from "react-router";
 import SocialLogin from "./SocialLogin";
+import axios from "axios";
 
 const Register = () => {
   const { createUser } = useAuth();
@@ -22,19 +23,53 @@ const Register = () => {
       .catch((error) => console.error(error));
   };
 
+  const handleImageUpload = async(e) => {
+    const image = e.target.files[0];
+    console.log(image);
+    const formData = new FormData();
+    formData.append("image", image);
+
+    const res=await axios.post(`https://api.imgbb.com/1/upload?expiration=600&key=YOUR_CLIENT_API_KEY`)
+  };
+
   return (
     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
       <div className="card-body">
         <h1 className="text-5xl font-bold">Register now!</h1>
         <form action="" onSubmit={handleSubmit(onSubmit)}>
           <fieldset className="fieldset">
+            <label className="label">Name</label>
+            <input
+              type="text"
+              className="input"
+              {...register("name", { required: true })}
+              placeholder="Name"
+            />
+            {errors.name?.type === "required" && (
+              <p className="text-red-500">Name is required</p>
+            )}
+
+            <label className="label">Your Profile Picture</label>
+            <input
+              type="file"
+              onChange={handleImageUpload}
+              className="input"
+              placeholder="Your Profile Picture"
+            />
+            {errors.name?.type === "required" && (
+              <p className="text-red-500">Name is required</p>
+            )}
+
             <label className="label">Email</label>
             <input
               type="email"
               className="input"
-              {...register("email")}
+              {...register("email", { required: true })}
               placeholder="Email"
             />
+            {errors.email?.type === "required" && (
+              <p className="text-red-500">Email is required</p>
+            )}
 
             <label className="label">Password</label>
             <input
